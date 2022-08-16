797df7ed240f33dba848203b4052788af39f9e42
EC2 Default User <ec2-user@ip-172-31-14-184.ec2.internal>
2022-08-16 18:56:56 +0000

    [edit]file

--- app/controllers/public/visions_controller.rb
+++ app/controllers/public/visions_controller.rb
@@ -1,17 +1,17 @@
 class Public::VisionsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_vision, only: [:show, :edit, :update, :destroy]
-  
+
   def new
     @vision = Vision.new
   end
-  
+
   def create
     @vision = Vision.new(vision_params)
     @vision.user_id = current_user.id
     @vision.save ? (redirect_to vision_path(@vision)) : (render :new)
   end
-  
+
   def index
     @all_visions = Vision.where(genre_id: params[:genre_id])
     if params[:genre_id].present?
@@ -21,21 +21,21 @@ class Public::VisionsController < ApplicationController
       @visions = Vision.all
     end
   end
-  
+
   def show
     @tasks = @vision.tasks.order(completion_on: "ASC")
     @tasks_still = @tasks.where(completion_status: false)
     @tasks_complete =@tasks.where(completion_status: true)
     @task = Task.new
   end
-  
+
   def edit
   end
 
   def update
     @vision.update(vision_params) ? (redirect_to vision_path(@vision)) : (render :edit)
   end
-  
+
   def destroy
     @vision.delete
     redirect_to user_path(current_user)
@@ -43,11 +43,11 @@ class Public::VisionsController < ApplicationController
 
 
   private
-  
+
   def vision_params
-    params.require(:vision).permit(:title, :body, :finish_on, :genre_id, :finish_status)
+    params.require(:vision).permit(:title, :body, :finish_on, :genre_id, :finish_status, :production)
   end
-  
+
   def ensure_vision
     @vision = Vision.find(params[:id])
   end
--- app/models/vision.rb
+++ app/models/vision.rb
@@ -3,26 +3,13 @@ class Vision < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   has_many :tasks, dependent: :destroy
-  
+
   validates :title, length: { minimum: 2, maximum: 20 }, presence: true
   validates :body, length: { maximum: 100 }
   validates :finish_on, presence: true
-  
+
   def self.search_for(content)
     Vision.where(['title LIKE ? OR body LIKE ?', "%#{content}%","%#{content}%"])
   end
-  
-  # def task_complete_count
-  #   @tasks = Vision.where{tasks(completion_status: true)}
-  #   @task_complete = @tasks.count
-  # end
-  
-  # def task_still_count
-  #   @tasks = Vision.where(task_completion_status: false)
-  #   @task_still = @tasks.counts
-  # end
-  
-  # def task_count
-  #   @tasks = Vision.Tasks.all
-  # end
+
 end
